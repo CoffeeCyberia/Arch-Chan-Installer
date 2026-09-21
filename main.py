a514f438e26f9ae3266f9a91da64448ba2b35a3b
CoffeeCyberia <david.ritschard@bluewin.ch>
2026-09-21 19:55:37 +0200

    added some new TUI elements

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import os
 from textual.app import App
-from textual.widgets import Footer, Header, Static, Button, Label
+from textual.widgets import Footer, Header, Static, Button, Label, TabbedContent, TabPane
 from textual.containers import Container
 from textual.screen import Screen
+import subprocess
 
 LOGO = """       
 
@@ -17,6 +18,7 @@ LOGO = """
 [#F85552]  .8'   `8. `88888.   [/#F85552][#DFA000] 8 8888         [/#DFA000][#8DA101]8 8888         [/#8DA101][#3A94C5] 8 8888         [/#3A94C5][#DF69BA]8 8888        [/#DF69BA]           [#35A77C]8 8888        [/#35A77C][#F57D26] ` 8888     ,8P[/#F57D26][#F85552] 8 8888         [/#F85552][#DFA000]8 8888         [/#DFA000]
 [#F85552] .888888888. `88888.  [/#F85552][#DFA000] 8 8888         [/#DFA000][#8DA101]8 8888         [/#8DA101][#3A94C5] 8 8888         [/#3A94C5][#DF69BA]8 8888        [/#DF69BA]           [#35A77C]8 8888        [/#35A77C][#F57D26]   8888   ,d8P [/#F57D26][#F85552] 8 8888         [/#F85552][#DFA000]8 8888         [/#DFA000]
 [#F85552].8'       `8. `88888. [/#F85552][#DFA000] 8 8888         [/#DFA000][#8DA101]8 8888         [/#8DA101][#3A94C5] 8 888888888888 [/#3A94C5][#DF69BA]8 888888888888[/#DF69BA]           [#35A77C]8 8888        [/#35A77C][#F57D26]    `Y88888P'  [/#F57D26][#F85552] 8 8888         [/#F85552][#DFA000]8 8888         [/#DFA000]
+
 """
 
 class Logo(Label):
@@ -40,8 +42,36 @@ class StartScreen(Screen):
 class SeconndScreen(Screen):
     def compose(self):
         yield Logo()
-        with Container(id="button1area"):
-            Label("[#5c6a72]On Which Devive do you want to Install Apple Puff?[/#5c6a72]")
+        with TabbedContent(classes="Installation"):
+            with TabPane("Locale", classes="InstallationTabs"):
+                with Container(id="button2area"):
+                     yield Label ("Test")
+            with TabPane("Device Selection", classes="InstallationTabs"):
+                with Container(id="button2area"):
+                    yield Label("[#5c6a72]On Which Devive do you want to Install Apple Puff?[/#5c6a72]",classes="DevSec")
+                    yield Label("[#F85552]!THE WHOLE DEVICE WILL BE EREASED![/#F85552]",classes="DevSec")
+                    # lsblk -o NAME,MODEL,SIZE ausgabe trennen so das jede zeile ein eintrag in einem array ist. So viele wie dann im array ist so viele macht es dann als yield
+                    # DiskNumber = 0
+                    # for i in Disks:
+                    #   yield Label(Disk[DiskNumber])
+                    #   DiskNumber + 1
+                    output = subprocess.check_output(
+                        ["lsblk", "-o", "NAME,MODEL,SIZE"],
+                        text=True
+                    )
+                    Disks = output.splitlines()
+
+                    for disk in Disks:
+                        yield Label(disk)
+            with TabPane("User Creation", classes="InstallationTabs"):
+                with Container(id="button2area"):
+                    yield Label ("Test")
+            with TabPane("Software", classes="InstallationTabs"):
+                with Container(id="button2area"):
+                    yield Label ("Test")
+            with TabPane("Summary", classes="InstallationTabs"):
+                with Container(id="button2area"):
+                    yield Label ("Test")       
 
 
 
